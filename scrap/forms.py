1c9acfd7cbaf704977f7d5904c6e5a19e37a1f8a
JunkyuPark <junk3843@naver.com>
2016-08-26 23:56:53 +0900

    add URL form UI that will be required for web scraping

---
+++ scrap/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class UrlForm(forms.Form):
+    url = forms.CharField(max_length=2000)
--- scrap/views.py
+++ scrap/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+import logging
 
+from .forms import UrlForm
 
 # Create your views here.
 
+logger = logging.getLogger(__name__)
+
+
 def main_view(request):
-    return render(request, 'scrap/main_view.html', {})
+    if request.method == 'POST': # 웹 스크래핑 버튼을 눌렀을 때
+        logger.warning('POST method')
+        """input 태그의 url 스크래핑을 시도한다."""
+    else:
+        logger.warning('GET method') # 새로고침을 했을 때
+        form = UrlForm()
+    return render(request, 'scrap/main_view.html', {'form': form})
+
